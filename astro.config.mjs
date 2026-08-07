5f84681e2dd7ff1abb7bc025933783903d5d0405
glorydazer63 <georget@glorydazeaor.com>
2026-08-08 09:58:14 +1200

    Sat08h

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ output: 'static',
 	  starlight({
 		  components: {
 			Footer: './src/components/CustomFooter.astro',
-					  }
+					  },
 			title: 'Notes and Pages',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
